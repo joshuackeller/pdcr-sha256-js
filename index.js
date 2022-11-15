158efb4e797a675d313acdaae9945fe2bc46a92a
joshuackeller <68955051+joshuackeller@users.noreply.github.com>
2022-11-15 08:52:59 -0700

    parallelizing

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const {readFileSync, promises: fsPromises} = require('fs');
 const { createHash } = require('crypto');
+const Parallel = require("paralleljs")
 
-const hashedPassword = "b3e8d5fcecc11814f64f9d9601733e2386a19a758c4d4d0d053a62a6baabc1cc"
+const hashedPassword = "12e3cccd824622dc43885e0a227cc1b8fc8ce9e5244f6a79c46dec048e3c52e3"
 
 async function asyncReadFile(filename) {
     try {
@@ -15,21 +16,31 @@ async function asyncReadFile(filename) {
     }
   }
 
+function getPassword (passwords) {
+  for (const password of passwords){
+    let hash = createHash('sha256').update(password).digest('hex');
+    if(hash == hashedPassword) {
+        console.log("Password found! The password is ", `"${password}"`)
+        return password
+    }
+  }
+}
+
 const main = async () => {
     const passwords = await asyncReadFile("./rockyou.txt")
     const start = Date.now()
-    for (let password of passwords){
-        let hash = createHash('sha256').update(password).digest('hex');
-        if(hash == hashedPassword) {
-            console.log("Password found! The password is ", `"${password}"`)
-            break
-        }
-    }
+
+    const p = new Parallel(passwords)
+
+    p.spawn(data => {
+      return getPassword(data)
+    }).then( data => console.log(data))
+
     const end = Date.now()
     
     const durationInSeconds = (end - start) / 1000
     const guessesPerSecond =  passwords.length / durationInSeconds
-    console.log(`The password was found in ${durationInSeconds}`)
+    console.log(`The password was found in ${durationInSeconds} seconds`)
     console.log("That's ", guessesPerSecond.toFixed(2), " per second")
 }
 
--- other.js
+++ other.js
@@ -5,4 +5,7 @@ const main = (newPassword) => {
     return createHash('sha256').update(newPassword).digest('hex');
 }
 
-console.log(main("0747260988malului"))
+console.log(main("colosalacristi"))
+
+// password - 5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8
+// colosalacristi - 12e3cccd824622dc43885e0a227cc1b8fc8ce9e5244f6a79c46dec048e3c52e3
